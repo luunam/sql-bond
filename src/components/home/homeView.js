5b8c25696d4c03a01e4ddd6ef36adc66891a5518
luunam <namluunhat164@gmail.com>
2017-01-28 19:50:54 -0500

    fix select bug

--- src/components/home/homeView.js
+++ src/components/home/homeView.js
@@ -19,7 +19,6 @@ function homeService() {
 }
 
 function homeController($scope, $location, homeService) {
-    console.log('PANEL SUCCESS');
 
     var connection = homeService.getValue();
 
@@ -27,8 +26,6 @@ function homeController($scope, $location, homeService) {
         if (err != null) {
             console.log('ERROR: ' + err);
         } else {
-            console.log(rows);
-            console.log(cols);
             $scope.databases = rows.map(function(row) {
                return row.Database;
             });
@@ -55,16 +52,12 @@ function homeController($scope, $location, homeService) {
     }
 
     function changeDatabase() {
-        console.log('Database changed');
-        console.log($scope.selectedDatabase);
-        console.log(typeof $scope.selectedDatabase);
+        $scope.selected = [];
         connection.query("use " + $scope.selectedDatabase + ";", function(err, rows, cols) {
             if (err != null) {
                 console.log('ERROR: ' + err);
             } else {
                 connection.query("show tables;", function(err, rows, cols) {
-                    console.log(rows);
-                    console.log(cols);
                     $scope.tables = rows.map(function(table) {
                         return { name: table['Tables_in_' + $scope.selectedDatabase] }
                     })
@@ -75,7 +68,7 @@ function homeController($scope, $location, homeService) {
     }
 
     function selectTable(tableIdx) {
-
+        $scope.selected = [];
         $scope.tableName = $scope.tables[tableIdx].name;
         connection.query("select * from " + $scope.tableName + ";", function(err, rows, cols) {
             $scope.$apply(function() {
@@ -84,11 +77,6 @@ function homeController($scope, $location, homeService) {
                 });
                 $scope.tableData = rows;
             });
-
-
-            console.log($scope.colNames);
-
-
         })
     }
 
