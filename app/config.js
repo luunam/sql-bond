3afbdca87b6803f81c52c76ea5a72603c3c19ec5
luunam <namluunhat164@gmail.com>
2017-03-28 02:38:53 -0400

    fix angular route version

--- app/config.js
+++ app/config.js
@@ -1,12 +1,13 @@
 (function () {
-  angular.module('sqlBond')
+  angular
+    .module('sqlBond')
     .config(['$mdThemingProvider', function ($mdThemingProvider) {
       'use strict';
 
       $mdThemingProvider.theme('default')
         .primaryPalette('blue');
     }])
-    .config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
       $routeProvider
         .when('/', {
           templateUrl: 'components/views/login/templates/login.view.html',
